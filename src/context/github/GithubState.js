be25b1392bead1aa00de8005c8cdecd2581da7f6
[michael norman] <[michaelnorman4421@gmail.com]>
2021-10-10 01:35:27 +0100

    setup build files and edited env for publishing- project complete

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,14 @@ import {
 	GET_REPOS,
 } from '../types';
 
+let githubClientToken;
+
+if (process.env.NODE_ENV !== 'production') {
+	githubClientToken = process.env.REACT_APP_GITHUB_TOKEN;
+} else {
+	githubClientToken = process.env.GITHUB_TOKEN;
+}
+
 const GithubState = (props) => {
 	const initialState = {
 		users: [],
@@ -23,7 +31,7 @@ const GithubState = (props) => {
 	const github = axios.create({
 		baseURL: 'https://api.github.com',
 		timeout: 1000,
-		headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
+		headers: { Authorization: githubClientToken },
 	});
 
 	//search users
